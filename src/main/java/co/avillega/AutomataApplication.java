75510be65f9357c99c79fecd364f01fe0a1172a5
avillega <villegas.pelaez.andres@gmail.com>
2017-03-22 07:40:56 -0500

    Initial commit

---
+++ src/main/java/co/avillega/AutomataApplication.java
@@ -0,0 +1,12 @@
+package co.avillega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomataApplication.class, args);
+	}
+}
